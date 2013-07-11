232a8fd09f0bb60c3ecb58feff552b79ac605e8f
Niklas Hedlund <nojan1989@gmail.com>
2013-07-11 21:53:31 +0200

    Added support for on the fly texture rotation with caching

--- textureset.py
+++ textureset.py
@@ -10,6 +10,8 @@ class TextureSet(object):
         if colorkey != None:
             self.image.set_colorkey(colorkey)
 
+        self.rotationCache = {}
+
     def getSingle(self, offset, size):
         return self.image.subsurface(Rect(offset, size))
 
@@ -17,3 +19,19 @@ class TextureSet(object):
         (x, y) = coords
         newOffset = (offset[0] + size[0] * x, offset[1] + size[1] * y)
         return self.getSingle(newOffset, size)
+        
+    def getSingleRotated(self, offset, size, angle):
+        searchKey = str(offset) + str(size) + str(angle)
+
+        if searchKey in self.rotationCache:
+            return self.rotationCache[searchKey]
+        else:
+            image = self.getSingle(offset, size)
+            transform.rotate(image, angle)
+            self.rotationCache[searchKey] = image
+            return image
+
+    def getSeriesRotated(self, offset, size, coords, angle):
+        (x, y) = coords
+        newOffset = (offset[0] + size[0] * x, offset[1] + size[1] * y)
+        return self.getSingleRotated(newOffset, size, angle)

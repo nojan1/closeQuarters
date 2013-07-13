e424fe87be6a6a97a05c50e3c87d55ef3a80a6f8
Niklas Hedlund <nojan1989@gmail.com>
2013-07-13 14:07:47 +0200

    Added missing file

---
+++ backgroundmusic.py
@@ -0,0 +1,27 @@
+from config import*
+
+from pygame import *
+import os
+import random
+
+class BackgroundMusic(object):
+    def __init__(self):
+        #Load all the filenames from music folder
+        self.files = []
+        for f in os.listdir(MUSICPATH):
+            if f != "." and f != "..":
+                self.files.append(f)
+
+        #Pygame event system will be used to trigger new track
+        mixer.music.set_endevent(USEREVENT)
+
+    def playTrack(self):
+        audioFile = self.files[ random.randint(0, len(self.files)) ]
+        mixer.music.load(os.path.join(MUSICPATH, audioFile))
+        mixer.music.play()
+
+    def pause(self):
+        mixer.music.pause()
+
+    def unpause(self):
+        mixer.music.unpause()

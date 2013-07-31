701d94b675546e66300633d2b723410febfbd81c
Niklas Hedlund <nojan1989@gmail.com>
2013-07-31 18:06:06 +0200

    Added cli arguments for control of res, fullscreen and fps counter

--- main.py
+++ main.py
@@ -15,9 +15,22 @@
 from core import Core
 from menu import *
         
-if __name__ == "__main__":
-    #core = Core((800,600), False)
-    core = Core((1024,768), False, drawFPS=True)
+import argparse
+
+if __name__ == "__main__":    
+    parser = argparse.ArgumentParser(description='2D top view zombie shooter game in cramped coridors', epilog='Have fun shooting zombies :)')
+    parser.add_argument("--draw-fps", help="Display FPS counter in upper left corner", action="store_true")
+    parser.add_argument("-r", "--resolution", help="The size of game window to use. Warning changing this will break the layout of the menu and dialogs", default="1024x768")
+    parser.add_argument("-f", "--fullscreen", help="Run in fullscreen", action="store_true")
+    args = parser.parse_args()
+
+    if "x" in args.resolution:
+        res = tuple( [int(x) for x in args.resolution.split("x")] )
+    else:
+        print("Warning: Unsupported resolution format defaulting to 1024x768")
+        res = (1024, 768)
+
+    core = Core(res, args.fullscreen, args.draw_fps)
     core.doInit()
     core.setActiveMode(Menu(core))
     core.enterLoop()
